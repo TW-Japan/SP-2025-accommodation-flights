85e8f3148004f2a756e453635141d1294c397f88
Trek-Works <github.travel@gmail.com>
2025-08-19 23:56:50 +1000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('sp-2025-cache').then(cache => {
+      return cache.addAll([
+        '/SP-2025-accommodation-flights/index.html',
+        '/SP-2025-accommodation-flights/assets/icons/icon-192x192.png',
+        '/SP-2025-accommodation-flights/assets/icons/icon-512x512.png',
+        '/SP-2025-accommodation-flights/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
